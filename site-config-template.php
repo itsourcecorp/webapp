084537ee57f1411a50a207c34b249be17068caa8
itsourcecorp <jsamos@itsourcecorp.net>
2012-08-01 11:54:41 -0700

    configure yii to use mysql

---
+++ site-config-template.php
@@ -0,0 +1,20 @@
+<?php
+define('APP_DEBUG', true);
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'DB_NAME');
+define('DB_USER', 'DB_USER');
+define('DB_PASSWORD', 'DB_PASSWORD');
+define('DB_HOST', 'DB_HOST');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+define('YMAIL_HOST', 'YMAIL_HOST');
+define('YMAIL_USR', 'YMAIL_USR');
+define('YMAIL_PWD', 'YMAIL_PWD');
+define('ADMIN_MAIL', 'ADMIN_MAIL');
+define('CONTACT_MAIL', 'CONTACT_MAIL');
+
+
+?>
