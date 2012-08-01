2a671eecf97d248337c0e912023b0346210dc348
itsourcecorp <jsamos@itsourcecorp.net>
2012-08-01 12:37:32 -0700

    add debug configuration

--- index.php
+++ index.php
@@ -7,7 +7,11 @@ $yii=dirname(__FILE__).'/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG',APP_DEBUG);
+if(APP_DEBUG){
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+}
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
