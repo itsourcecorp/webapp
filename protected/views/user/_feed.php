7d5edb8f201b99b3750bd9b2f83b2f5b42f4c6e0
itsourcecorp <jsamos@itsourcecorp.net>
2012-08-02 21:05:59 -0700

    more thumbs

--- protected/views/user/_feed.php
+++ protected/views/user/_feed.php
@@ -1,3 +1,8 @@
-
-<li class="span2"><img src="<?php echo $data['images']['thumbnail']['url']; ?>" /></li>
+<li class="span2">
+    <div class="thumbnail">
+        <h3><?php echo $data['user']['username'] ?></h3>
+        <img src="<?php echo $data['images']['thumbnail']['url']; ?>" />
+        <div><i class="icon-thumbs-up"></i></div>
+    </div>
+</li>
 <!-- <span><?php  var_dump( $data['caption'] ) ?> </span>   -->
