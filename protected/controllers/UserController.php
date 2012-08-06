d4346f650d1f176cdefb2f2e5df5776c2b3f3a72
itsourcecorp <jsamos@itsourcecorp.net>
2012-08-06 08:26:37 -0700

    UserController extends OuathController

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-class UserController extends Controller
+class UserController extends OuathController
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
